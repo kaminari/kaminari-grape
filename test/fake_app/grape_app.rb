304bae248cf24fdd03513055886c116fdaaf1866
Akira Matsuda <ronnie@dio.jp>
2018-01-04 10:25:39 +0900

    This gem doesn't load railties, so database.yml won't be loaded in the tests here

--- test/fake_app/grape_app.rb
+++ test/fake_app/grape_app.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+ActiveRecord::Base.configurations = {'test' => {'adapter' => 'sqlite3', 'database' => 'kaminari_test.sqlite3'}} if ActiveRecord::Base.configurations.empty?
 require 'fake_app/active_record/models'
 
 class GrapeApp < ::Grape::API
